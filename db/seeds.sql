472f686df3352949a9078bbc64398ec34ddfeba6
upstairskid <45738525+upstairskid@users.noreply.github.com>
2019-04-30 01:08:59 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES('Big Mac');
+INSERT INTO burgers (burger_name) VALUES('Double Big Mac');
+INSERT INTO burgers (burger_name) VALUES('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES('Double Cheese Burger');
